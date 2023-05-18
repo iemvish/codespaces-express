637b626491eb76eec8964859a39cfe5f381918df
Vishal Kumar <92718888+iemvish@users.noreply.github.com>
2023-05-18 13:24:49 +0000

    demoWorkspace

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
+app.get('/user', (req, res) => {
+  res.send('I am user')
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
